e45fb02b7da7b3aee3c2431def2bcebb7baaff5e
owensgro <149115981+owensgro@users.noreply.github.com>
2025-09-19 12:28:36 +0000

    Added argvcat files

--- Lab3/argvcat.c
+++ Lab3/argvcat.c
@@ -9,27 +9,42 @@ void my_error(char *s)
     exit(1);
 }
 
-/* Concatnate two strings.
+/* Concatenate two strings.
  * Dynamically allocate space for the result.
  * Return the address of the result.
  */
 char *my_strcat(char *s1, char *s2)
 {
-    // TODO 
-    return NULL;
+    size_t len1 = strlen(s1);
+    size_t len2 = strlen(s2);
+
+    char *result = malloc(len1 + len2 + 1);
+    if (result == NULL) {
+        my_error("malloc failed");
+    }
+
+    strcpy(result, s1);
+    strcat(result, s2);
+
+    return result;
 }
 
 int main(int argc, char *argv[])
 {
-    char    *s;
+    char *s;
+    char *tmp;
+
 
     s = my_strcat("", argv[0]);
 
-    for (int i = 1; i < argc; i ++) {
-        s = my_strcat(s, argv[i]);
+    for (int i = 1; i < argc; i++) {
+        tmp = my_strcat(s, argv[i]);
+        free(s);      // free old string
+        s = tmp;      // update to new string
     }
 
     printf("%s\n", s);
 
+    free(s); // free final string
     return 0;
 }
